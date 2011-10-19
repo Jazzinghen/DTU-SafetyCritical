b67205563310df927166225a25d7f74871f59630
Filip G <groszynskif@gmail.com>
2011-10-19 22:12:14 +0200

    NextBitPermutation

--- main.c
+++ main.c
@@ -12,19 +12,19 @@ void Test(void) {
 int main(int argc, char** argv) {
 	uint8_t i;
 	
-	for (i = 0; i<32; i++) {
+	uint32_t v = 3; // current permutation of bits 
+	for ( i = 0; i < 100; i++) {
+		PrintBinary(v);
+		v = NextBitPermutation(v);
+	}
+	
+/*	for (i = 0; i<32; i++) {
     	PrintBinary(RotR(0x55,i));
 	}	
 	for (i = 0; i<32; i++) {
     	PrintBinary(RotL(0x55,i));
-	}
-	
-    printf("%d\n", GetParity(0));
-    printf("%d\n", GetParity(0x1));
-    printf("%d\n", GetParity(0x2));
-    printf("%d\n", GetParity(0x3));
-    printf("%d\n", GetParity(0xf4));
-    printf("%d\n", GetParity(0xffff));
+	}*/
+
 
     PrintBinary(GneratorPoly);
     PrintBinary(0x555<<11);
