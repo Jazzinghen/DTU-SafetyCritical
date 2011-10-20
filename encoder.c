be38bc8152055c4f7bbe77b98830a05af074a97d
Michele Bianchi <jazzinghen@gmail.com>
2011-10-20 20:21:07 +0200

    Added Parity modes to encoding. :3

--- encoder.c
+++ encoder.c
@@ -1,7 +1,8 @@
 #include "headers/utils.h"
 
-uint32_t Encode(GolayCW *CodeWord) {
+uint32_t Encode(uint8_t parity_mode, GolayCW *CodeWord) {
 	CodeWord->cw.check = GetSyndrome(CodeWord->cw.data)&0x7ff;
-  CodeWord->cw.parity = GetParity(CodeWord->CodeWord);
-
+  if (parity_mode == GOLAY_24){
+    CodeWord->cw.parity = GetParity(CodeWord->CodeWord);
+  }
 }
--- headers/encoder.h
+++ headers/encoder.h
@@ -1,4 +1,8 @@
 #ifndef ENCODER_H_INCLUDED
 #define ENCODER_H_INCLUDED
 
+#include <stdint.h>
+
+uint32_t Encode (uint8_t parity_mode, GolayCW *CodeWord);
+
 #endif // ENCODER_H_INCLUDED
