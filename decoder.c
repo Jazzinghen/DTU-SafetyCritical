797bfda0b97b7b9774c63c53c26397b683838248
Michele Bianchi <jazzinghen@gmail.com>
2011-10-20 17:59:16 +0200

    Finished rewriting all the code for Correction / Detection.
    I just hope everything works fine after this -_-'

--- decoder.c
+++ decoder.c
@@ -16,8 +16,8 @@ uint8_t decode(uint8_t correct_mode, uint8_t *errors, GolayCW *codeWord){
       parity_bit = codeWord->cw.parity; /* save parity bit */
       codeWord->cw.parity = 0;            /* remove parity bit for correction */
 
-      codeWord->CodeWord = Correction(codeWord->CodeWord, errors);     /* correct up to three bits */
-      codeWord->CodeWord = parity_bit;            /* restore parity bit */
+      errors = Correction(codeWord);     /* correct up to three bits */
+      codeWord->cw.parity = parity_bit;            /* restore parity bit */
 
       /* check for 4 bit errors */
       if (GetParity(codeWord->CodeWord) > 0)            /* odd parity is an error */
@@ -42,6 +42,61 @@ uint8_t decode(uint8_t correct_mode, uint8_t *errors, GolayCW *codeWord){
     }
 } /* decode */
 
-void Correction(void) {
+/* This function corrects Golay [23,12] codeword cw, returning the
+   corrected codeword. This function will produce the corrected codeword
+   for three or fewer errors. It will produce some other valid Golay
+   codeword for four or more errors, possibly not the intended
+   one. *errs is set to the number of bit errors corrected. */
 
-}
+uint8_t Correction (GolayCW *codeWord)
+{
+  uint8_t limitWeight;                /* current syndrome limit weight, 2 or 3 */
+  uint32_t mask;             /* mask for bit flipping */
+  int16_t
+    i,j;              /* index */
+  uint32_t syndrome;                /* calculated syndrome */
+  GolayCW tempCW;          /* saves initial value of cw */
+
+  uint8_t errors = 0;
+
+  tempCW = *codeWord;         /* save */
+  limitWeight = 3;                /* initial syndrome weight threshold */
+  j = -1;               /* -1 = no trial bit flipping on first pass */
+  mask = 1;
+  while (j < 23) /* flip each trial bit */
+    {
+      if (j != -1) /* toggle a trial bit */
+        {
+          if (j > 0) /* restore last trial bit */
+            {
+              codeWord->CodeWord = tempCW.CodeWord ^ mask;
+              mask += mask; /* point to next bit */
+            }
+          codeWord->CodeWord = tempCW.CodeWord ^ mask; /* flip next trial bit */
+          limitWeight = 2; /* lower the threshold while bit diddling */
+        }
+
+      syndrome = GetSyndrome(codeWord->CodeWord); /* look for errors */
+      if (syndrome > 0) /* errors exist */
+        {
+          for (i=0; i<23; i++) {/* check syndrome of each cyclic shift */
+              if ((errors = Weight(syndrome)) <= limitWeight) {
+                  codeWord->CodeWord = codeWord->CodeWord ^ syndrome;              /* remove errors */
+                  codeWord->CodeWord = RotR(codeWord->CodeWord, i);  /* unrotate data */
+                  return(errors);
+                }
+              else
+                {
+                  codeWord->CodeWord = RotL(codeWord->CodeWord,1);   /* rotate to next pattern */
+                  syndrome = GetSyndrome(codeWord->CodeWord);         /* calc new syndrome */
+                }
+            }
+          j++; /* toggle next trial bit */
+        }
+      else
+        return(errors); /* return corrected codeword */
+    }
+
+  *codeWord = tempCW;
+  return(errors); /* return original if no corrections */
+} /* correct */
