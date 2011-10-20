dc4a2a35184e52fa0bf57e08e562ecf3a682671b
Filip G <groszynskif@gmail.com>
2011-10-20 17:41:28 +0200

    fixed

--- encoder.c
+++ encoder.c
@@ -1,5 +1,5 @@
 #include "headers/utils.h"
 
-uint32_t Encode(uint32_t CodeWord) {
-	//return ((GetSyndrome(CodeWord)<<12)|CodeWord)
+uint32_t Encode(GolayCW *CodeWord) {
+	CodeWord->cw.check = GetSyndrome(CodeWord->cw.data);
 }
