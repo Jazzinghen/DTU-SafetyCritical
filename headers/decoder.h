0d6ebb4fc82492fa21119f45c70c3faf341782a8
Filip G <groszynskif@gmail.com>
2011-10-21 15:15:14 +0200

    Added declaration of lookup tables and file encoder/decoder

--- headers/decoder.h
+++ headers/decoder.h
@@ -3,8 +3,9 @@
 
 #include "utils.h"
 
-uint8_t ErrorCheck (uint8_t parity_mode, GolayCW *codeWord);
+uint8_t Decode (uint8_t correct_mode, uint8_t *errors, GolayCW *codeWord);
+uint8_t DecodeFile (char *src, char *dst, uint8_t mode);
 
-uint8_t Correction (uint8_t parity_mode, GolayCW *codeWord);
+uint8_t Correction (GolayCW *codeWord);
 
 #endif // DECODER_H_INCLUDED
